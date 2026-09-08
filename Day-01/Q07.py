db32cd3e6704802c4e9e82181701fc8c6a972c97
vikasyadav91400 <vikasyadav91400@gmail.com>
2026-09-08 22:30:43 +0530

    Add to day's work

---
+++ Day-01/Q07.py
@@ -0,0 +1,9 @@
+#division of two numbers
+x=10
+y=2
+print(x/y)
+
+#user input
+x=input("Enter first number: ")
+y=input("Enter second number:") 
+print("The division of numbers is: ", int(x)/int(y))

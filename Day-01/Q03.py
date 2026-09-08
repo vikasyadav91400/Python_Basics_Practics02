f1e37759b7b5e1828f303a6fc1b8c6bb2367573b
vikasyadav91400 <vikasyadav91400@gmail.com>
2026-09-08 22:22:01 +0530

    Add to day's work

--- Day-01/Q03.py
+++ Day-01/Q03.py
@@ -1,7 +1,12 @@
-#user input
+# #user input
 name=input("Enter your name: ")
 print(name)
 
-table=int(input("Enter the table number: "))
-print(table)
+# table=int(input("Enter the table number: "))
+# print(table)
         
+age=input("Enter age: ")
+print(type(age))     
+
+marks=input("Enter marks: ")
+print(type(marks))   
---
+++ Day-01/Q05.py
@@ -0,0 +1,9 @@
+#diffrence of two numbers
+x=8
+y=5
+print(x-y)
+
+#user input
+x=input("Enter first number: ")
+y=input("Enter second number:")
+print("The difference of numbers is: ", int(x)-int(y))

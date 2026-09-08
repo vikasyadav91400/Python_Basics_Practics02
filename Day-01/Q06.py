e2b90ab7213630b4c8920a019bb575936b9781cd
vikasyadav91400 <vikasyadav91400@gmail.com>
2026-09-08 22:28:39 +0530

    Add to day's work

---
+++ Day-01/Q06.py
@@ -0,0 +1,9 @@
+#multiplication of ywo numbers
+x=5
+y=4
+print(x*y)
+
+#user input
+x=input("Enter first number: ")
+y=input("Enter second number:") 
+print("The multiplication of numbers is: ", int(x)*int(y))

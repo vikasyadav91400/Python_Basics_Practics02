377ad3d455220f57abe7368ed54d25a31620cc09
vikasyadav91400 <vikasyadav91400@gmail.com>
2026-09-08 09:15:03 +0530

    Add to day's work

---
+++ Day-01/Q04.py
@@ -0,0 +1,9 @@
+#find the sum of two numbers
+a=5
+b=4
+print(a+b)
+
+#user input
+a=input("Enter the first number: ")
+b=input("Enter the second number: ") 
+print("The sum of numbers is: ", int(a)+int(b))

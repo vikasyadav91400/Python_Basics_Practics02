3cf910d6c61a195318028418562485a525300ef6
vikasyadav91400 <vikasyadav91400@gmail.com>
2026-09-11 09:25:38 +0530

    Add today's work

---
+++ Day-02/Q15.py
@@ -0,0 +1,23 @@
+#smallest number among three numbers
+
+a=-4
+b=0
+c=8
+if a < b and a < c:
+    print("a is smallest number")
+elif b < a and b < c:
+    print("b is smallest number")
+else:
+    print("c is smallest number")    
+    
+#user input
+ 
+a=int(input("Enter first number:"))  
+b=int(input("Enter second number:"))
+c=int(input("Enter third number:"))
+if a < b and a < c:
+    print("a is smallest number")
+elif b < a and b < c:
+    print("b is smallest number")
+else:
+    print("c is smallest number")          

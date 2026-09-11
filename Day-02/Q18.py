96ab4791a2a7ba91b2044b9e304ac041e6b007ea
vikasyadav91400 <vikasyadav91400@gmail.com>
2026-09-11 09:46:46 +0530

    Add today's work

---
+++ Day-02/Q18.py
@@ -0,0 +1,6 @@
+#calculate simple interest
+p=int(input("Enter principal amount:"))
+r=int(input("Enter rate of interest:"))
+t=int(input("Enter time in years:"))
+si=(p*r*t)/100
+print("Simple Interest is:",si)

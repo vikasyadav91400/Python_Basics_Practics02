92a8b465cb2efcdcde3be6151509b21b547b2e6e
vikasyadav91400 <vikasyadav91400@gmail.com>
2026-09-11 09:07:52 +0530

    Add today's work

---
+++ Day-02/Q13.py
@@ -0,0 +1,17 @@
+# greatest of two numbers
+a = 10
+b = 20
+if a > b:
+    print("a is greater than b")
+else:
+    print("b is greater than a")
+    
+ #user input  
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+if a > b:
+    print("a is grater")
+else:
+    print("b is grater")    
+          
+           

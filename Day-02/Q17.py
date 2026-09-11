f1b877f91b13043b4cc1ca2404404fb6a4841596
vikasyadav91400 <vikasyadav91400@gmail.com>
2026-09-11 09:38:38 +0530

    Add today's work

---
+++ Day-02/Q17.py
@@ -0,0 +1,14 @@
+#calculate a students percentage from marks
+a=int(input("Enter first subject marks:"))
+b=int(input("Enter second subject marks:"))
+c=int(input("Enter third subject marks:"))
+total=a+b+c
+percentage=(total/300)*100
+print("Percentage of student is:",percentage)
+
+a=90
+b=68
+c=89
+total = a+b+c
+percentage=(total/300)*100
+print("Percentage of student is:",percentage)

7306d55eca38223626caf0200fb9eb8e5c0f5a34
vikasyadav91400 <vikasyadav91400@gmail.com>
2026-09-11 09:19:37 +0530

    Add today's work

---
+++ Day-02/Q14.py
@@ -0,0 +1,11 @@
+#largest of three numbers
+a=80
+b=50
+c=40
+if a > b and a > c:
+    print("a is the largest")
+elif b > a and b > c:
+    print("b is the largest number")
+else:
+    print("c is the largest number")
+    

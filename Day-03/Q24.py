eec947d14748d81e9652bd042868556b89a35910
vikasyadav91400 <vikasyadav91400@gmail.com>
2026-09-12 10:23:18 +0530

    Add today's work

---
+++ Day-03/Q24.py
@@ -0,0 +1,15 @@
+#vowel or consonant
+#user input
+ch=input("Enter a character: ")
+if ch in ['a', 'e', 'i', 'o', 'u']:
+    print("The character is a vowel")
+else:
+    print("The character is a consonant")
+    
+    
+#hard coded
+ch='a', 'e', 'i', 'o', 'u'
+if ch in ['a', 'e', 'i', 'o', 'u']:
+    print("The character is a vowel")
+else:
+    print("The character is a consonant")
---
+++ Day-03/Q25.py
@@ -0,0 +1,17 @@
+#pass or fail
+marks=70
+if marks >=75:
+    print("Student has pass")
+else:
+    print("Student has fail")    
+    
+    #user input
+marks=int(input("Enter your marks: "))
+if marks >=75:
+    print("Student has pass first division") 
+elif marks >=60:
+    print("Student has pass second division")
+elif marks >=50:
+    print("Student has pass third division")
+else:
+    print("Student has fail")               

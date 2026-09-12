a655de07f0367174dff870fe6b935a05ca84fa9e
vikasyadav91400 <vikasyadav91400@gmail.com>
2026-09-12 09:53:04 +0530

    Add today's work

---
+++ Day-03/Q21.py
@@ -0,0 +1,14 @@
+#check eligblity based on age
+age=18
+if age >= 18:
+    print ("You are eligible to work")
+else:
+    print ("You are not eligible to work")    
+    
+    
+#user input
+age=int(input("Enter your age: "))
+if age >=18:
+    print ("You are eligible to work")
+else:   
+    print ("You are not eligible to work")    
---
+++ Day-03/Q22.py
@@ -0,0 +1,11 @@
+#number divisible by both 3 and 5
+number = 18
+if number % 3 == 0 and number % 5 == 0:
+    print("The number is divisible by both 3 and 5")
+else:
+    print("The number is not divisible by both 3 and 5")
+    
+#user input
+number=int(input("Enter a number:"))
+if number % 3 == 0 and number % 5 == 0:
+    print("The number is divisible by both 3 and 5")    

0e085c37a7248038cf8df9bda4f53456a815eaa0
vikasyadav91400 <vikasyadav91400@gmail.com>
2026-09-13 13:55:39 +0530

    Add today's work

---
+++ Day-03/Q26.py
@@ -0,0 +1,22 @@
+#Program to check the grade of student based on marks
+marks=85
+if marks>=90:
+    print("Students has pass with grade A")
+elif marks>=80:
+    print("Students has pass with grade B")
+elif marks>=70:
+    print("Students has pass with grade C")
+else:
+    print("Students has fail")            
+    
+    #user input
+marks=int(input("Enter your marks: "))
+if marks>90:
+    print("Students has pass with grade A")
+elif marks>=80:
+    print("Students has pass with grade B")
+elif marks>=70:
+    print("Students has pass with grade C")
+else:
+    print("Students has fail")                
+    

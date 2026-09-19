e4a8bb02478d539c933f8d1b991f8c6a2aada003
vikasyadav91400 <vikasyadav91400@gmail.com>
2026-09-19 10:31:33 +0530

    Add today's work

---
+++ Day-04/Q36.py
@@ -0,0 +1,8 @@
+#find the miltiplication table of N
+n = int(input("Enter the value of N: "))
+for i in range(1, 11):
+    print(n, "x", i, "=", n * i)
+    
+n=5
+for i in range(1, 11):
+    print(n, "x", i, "=", n * i)
